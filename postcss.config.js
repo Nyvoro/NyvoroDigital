a5889358f79d3fe379dbe4a2af7d05c6e0fdbd1e
Robbe1991 <RobinThonhofer@gmx.de>
2025-07-04 22:06:46 +0000

    holy moly

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/** PostCSS Konfiguration für Tailwind v4 + Next 15  */
+module.exports = {
+  plugins: [
+    '@tailwindcss/postcss',   // ➜ neues offizielles Plugin ab Tailwind 4
+    'autoprefixer',
+  ],
+};
